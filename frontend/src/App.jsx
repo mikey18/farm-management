75e15a1190319d22b163e90df89def0c69d3316a
miqit <mimarkethq@gmail.com>
2025-03-09 08:11:46 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Layout from './components/Layout';
-import InventoryManagement from './pages/InventoryManagement';
-import LivestockManagement from './pages/LivestockManagement';
-import CropManagement from './pages/CropManagement';
-import FinancialManagement from './pages/FinancialManagement';
-import CropPrediction from './pages/CropPrediction';
+// import InventoryManagement from './pages/InventoryManagement';
+// import LivestockManagement from './pages/LivestockManagement';
+// import CropManagement from './pages/CropManagement';
+// import FinancialManagement from './pages/FinancialManagement';
+// import CropPrediction from './pages/CropPrediction';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -13,11 +13,11 @@ function App() {
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Dashboard />} />
-                <Route path="inventory" element={<InventoryManagement />} />
-                <Route path="livestock" element={<LivestockManagement />} />
+                {/* <Route path="inventory" element={<InventoryManagement />} /> */}
+                {/* <Route path="livestock" element={<LivestockManagement />} />
                 <Route path="crops" element={<CropManagement />} />
                 <Route path="finances" element={<FinancialManagement />} />
-                <Route path="prediction" element={<CropPrediction />} />
+                <Route path="prediction" element={<CropPrediction />} /> */}
                 <Route path="*" element={<NotFound />} />
             </Route>
         </Routes>
